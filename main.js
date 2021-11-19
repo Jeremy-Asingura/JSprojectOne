4c0a330ece76b921d3e2ee0b4062845ec2f6b010
Jeremy Asingura <blessedjosh234@gmail.com>
2021-11-19 12:46:46 +0300

    added two files

---
+++ main.js
@@ -0,0 +1,9 @@
+// ! to ask for one's first name
+
+let firstName = prompt("What is your first name?")
+alert(`You are most welcome ${firstName}`)
+
+// ! to ask for one's second name
+
+let secondName = prompt("What is your second name?")
+alert(`You are most welcome ${firstName} ${secondName}`)
