75b42ecaf8763bf840f6c39e510e168736270697
Jeremy Asingura <blessedjosh234@gmail.com>
2021-11-19 15:58:27 +0300

    added more js and css code

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 // ! to ask for one's first name
 
-let firstName = prompt("What is your first name?")
-alert(`You are most welcome ${firstName}`)
+let firstName = prompt("What is your first name?");
+alert(`You are most welcome ${firstName}`);
 
 // ! to ask for one's second name
 
-let secondName = prompt("What is your second name?")
-alert(`You are most welcome ${firstName} ${secondName}`)
+let secondName = prompt("What is your second name?");
+alert(`You are most welcome ${firstName} ${secondName}`);
+
+// ! to ask one for their age
+
+let age = prompt("How old are you?");
+
+// ! to determine whether they are a child or an adult
+
+if (age < 18) {
+    alert(`You are not an adult.}`)
+} else {
+    alert(`You are an adult.`)
+}
